cc8beb9d1534ff65d8ac70eab046f3e6d4dd18a7
kmannarelli <kmannarelli@info-212-02x.iut.bx1>
2021-09-06 13:17:29 +0200

    Commit exercice 2 fin (ajout de la classe Salle et des méthodes Equals et HashCode)

---
+++ src/main/java/labyrinthe/Salle.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labyrinthe;
+
+/**
+ *
+ * @author kmannarelli
+ */
+public class Salle implements ISalle{
+    private int x;
+    private int y;
+    
+    public Salle(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    
+    public int getX(){
+       return x; 
+    }
+        
+
+    public int getY(){
+        return y; 
+    }
+    
+    public void setX(int newX){
+        this.x=newX;
+    }
+    
+    public void setY(int newY){
+        this.y=newY;
+    }
+
+    public boolean estAdjacente(ISalle autre){
+        return false; 
+    }
+    @Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + x;
+			result = prime * result + y;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			Salle other = (Salle) obj;
+			if (getX() != other.getX() )
+				return false;
+			if (getY() != other.getY())
+				return false;
+			return true;
+		}
+	   
+}
