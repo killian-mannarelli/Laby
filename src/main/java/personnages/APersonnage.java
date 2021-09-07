abe9b93c6e039e7c52cd0bc0676587ef87ee1761
kmannarelli <killian.mannarelli@etu.u-bordeaux.fr>
2021-09-07 21:50:41 +0200

    Fin exercice 11 début exercice 12

---
+++ src/main/java/personnages/APersonnage.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personnages;
+
+import java.util.Collection;
+import labyrinthe.ISalle;
+
+/**
+ *
+ * @author Epsi
+ */
+public abstract class APersonnage implements IPersonnage{
+    private ISalle position;
+    
+     
+
+    // renvoie sa position courante
+    @Override
+    public ISalle getPosition(){
+        return position;
+    }
+        
+    
+    
+    // definit sa position courante
+    @Override
+    public void setPosition( ISalle s) {
+        position = s;
+    }
+    @Override
+     public abstract ISalle faitSonChoix(Collection<ISalle> sallesAccessibles);
+    
+}
---
+++ src/main/java/personnages/Heros.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personnages;
+
+import java.util.Collection;
+import java.util.Scanner;
+import labyrinthe.ISalle;
+
+/**
+ *
+ * @author Epsi
+ */
+public class Heros extends APersonnage{
+   Scanner sc=null;
+   @Override
+     public ISalle faitSonChoix(Collection<ISalle> sallesAccessibles){
+         
+     }
+}
